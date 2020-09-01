24e076dc378cf4780f42778ed90fdd781ab831c6
chijiokemekowulu <c.mekowulu@yahoo.com>
2020-09-01 21:36:55 +0100

    commiting

--- stock_simulator/stock_simulator_app/models.py
+++ stock_simulator/stock_simulator_app/models.py
@@ -3,20 +3,17 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
-    balance = models.ForeignKey(Money, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
-
-
 class Stock(models.Model):
     stock_name = models.CharField(max_length=30)
     stock_value = models.IntegerField(max_length=30)
 
 
-class Money(models.Model):
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     balance = models.IntegerField(default=20000)
+
+    def __str__(self):
+        return self.user.username
+
