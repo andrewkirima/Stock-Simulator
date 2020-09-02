1d40f65a91a7b360d095107a4019a694dc69639a
chijiokemekowulu <c.mekowulu@yahoo.com>
2020-09-02 14:37:27 +0100

    third

--- stock_simulator/stock_simulator/urls.py
+++ stock_simulator/stock_simulator/urls.py
@@ -20,7 +20,7 @@ from stock_simulator_app.views import Login, Logout, Record
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontend.urls')),
-    path('addUser/', Record.as_view(), name="register"),
+    path('register/', Record.as_view(), name="register"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
 ]
--- stock_simulator/stock_simulator_app/urls.py
+++ stock_simulator/stock_simulator_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import Record, Login, Logout
 
 urlpatterns = [
-    path('addUser/', Record.as_view(), name="register"),
+    path('register/', Record.as_view(), name="register"),
     path('login/', Login.as_view(), name="login"),
     path('logout/', Logout.as_view(), name="logout"),
 ]
